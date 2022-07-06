e454260142bccbfbf7b5d504b56426106e86dc8e
Denis Lykov <cailean393@gmail.com>
2022-07-06 04:05:54 +0300

    feature: console.log for error handler

--- server/src/map/errorHandler.ts
+++ server/src/map/errorHandler.ts
@@ -2,6 +2,7 @@ import { Request, Response, NextFunction } from "express";
 
 const errorHandler = (err: any, _: Request, res: Response, __: NextFunction) => {
   res.set("Content-Type", "text/plain");
+  console.error(err);
   if (process.env.NODE_ENV === "development") {
     if (err instanceof Error) {
       return res.status(500).send(err.message);
